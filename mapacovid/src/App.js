4c146c4611c33bbe0ec798943d3405819ae5bd89
EliazarMay <44253985+EliazarMay@users.noreply.github.com>
2020-05-07 17:58:34 -0500

    creacion mapa

--- mapacovid/src/App.js
+++ mapacovid/src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
+import { Icon } from "leaflet";
+import useSwr from "swr";
+import "./App.css";
 
-function App() {
+const fetcher = (...args) => fetch(...args).then(response => response.json());
+
+
+const icon = new Icon({
+  iconUrl: "/virus.svg",
+  iconSize: [25, 25]
+});
+
+
+export default function App() {
+  const url =
+    "https://data.police.uk/api/crimes-street/all-crime?lat=52.629729&lng=-1.131592&date=2019-10";
+  const { data, error } = useSwr(url, fetcher);
+  const cases = data && !error ? data.slice(0, 50) : [];
+  const [activeCrime, setActiveCrime] = React.useState(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Map center={[52.6376, -1.135171]} zoom={12}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+
+      {cases.map(crime => (
+        <Marker
+          position={[crime.location.latitude, crime.location.longitude]}
+          icon={icon}
+          onClick={() => {
+
+          }}
+        />
+      ))}
+    </Map>
   );
 }
-
-export default App;
