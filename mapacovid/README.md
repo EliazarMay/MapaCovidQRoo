c00f06fff0be8c16d88fccf0a2f4eb07384d3905
EliazarMay <44253985+EliazarMay@users.noreply.github.com>
2020-05-07 21:21:36 -0500

    modificación archivos svg

--- mapacovid/README.md
+++ mapacovid/README.md
@@ -1,21 +1,6 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
-## Available Scripts
-
-In the project directory, you can run:
-
-### `npm start`
-
-Runs the app in the development mode.<br />
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
-
-The page will reload if you make edits.<br />
-You will also see any lint errors in the console.
-
-### ### `npm init`
+### `Lista de para ejecutar la página`
+### `npm init`
 Cuando se clone el proyecto desde github es necesario instalar las dependecias
-
-
 ### `npm install swr`
 Instalar la liberia SWR para poder hacer uso de los datos de un json
 
@@ -24,3 +9,8 @@ Instalar la liberia leaflet para el mapa
 
 ### `npm install leaflet react react-dom`
 Liberia para el mapa con react
+
+### `npm start`
+Comandos necesarios para el proyecto
+
+### `Primer avance del proyecto`
--- mapacovid/src/App.js
+++ mapacovid/src/App.js
@@ -8,34 +8,56 @@ const fetcher = (...args) => fetch(...args).then(response => response.json());
 
 
 const icon = new Icon({
-  iconUrl: "/virus.svg",
-  iconSize: [25, 25]
+  iconUrl: "/cows.svg",
+  iconSize: [60, 60]
 });
 
+const pumas = new Icon({
+  iconUrl: "/Pumas.svg",
+  iconSize: [60, 60]
+});
 
 export default function App() {
-  const url =
+  /*const url =
     "https://data.police.uk/api/crimes-street/all-crime?lat=52.629729&lng=-1.131592&date=2019-10";
   const { data, error } = useSwr(url, fetcher);
   const cases = data && !error ? data.slice(0, 50) : [];
   const [activeCrime, setActiveCrime] = React.useState(null);
-
+  */
   return (
     <Map center={[21.1607406, -86.8613988]} zoom={12}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
-
-      {cases.map(crime => (
         <Marker
-          position={[crime.location.latitude, crime.location.longitude]}
+          position={[21.1607406, -86.8613988]}
           icon={icon}
           onClick={() => {
 
           }}
         />
-      ))}
-    </Map>
+        <Marker
+          position={[21.1756608, -86.8402514]}
+          icon={icon}
+          onClick={() => {
+
+          }}
+        />
+        <Marker
+          position={[21.177697, -86.8220446]}
+          icon={icon}
+          onClick={() => {
+
+          }}
+        />
+        <Marker
+          position={[21.1846515, -86.8532496]}
+          icon={pumas}
+          onClick={() => {
+
+          }}
+        />
+      </Map>
   );
 }
